f43a82f11b08d6d56c0dc5286a8e1857274fea16
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-13 17:27:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,18 +2,18 @@ class Anagram
 
 attr_accessor :word
 
-def initialize(word)
-  @word = word
-end
+  def initialize(word)
+    @word = word
+  end
 
-def match(array)
-  matches = []
-  array.each do |word|
+  def match(array)
+    matches = []
+    array.each do |word|
     if @word.split("").sort == word.split("").sort
       matches << word
     end
 
+    end
+    matches
   end
-  matches
-end
 end
